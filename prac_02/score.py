a33d3de8128ebe9615c1a62d72ff54be75c9eb2b
Damien Turner <damien.turner@my.jcu.edu.au>
2025-05-25 15:42:59 +1000

    Made minor correction/edits using pylint.

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,21 +1,22 @@
-# Broken Scores Solution
+"""Program to get a score from the user and print the corresponding status."""
+
 
 def main():
     """Get number score and print status"""
     score = float(input("Enter score: "))
-    print(assign_status(score))
+    print(determine_result(score))
 
 
-def assign_status(score):
-    """Assign status based on number score"""
+def determine_result(score):
+    """Assign status based on number score."""
     if score < 0 or score > 100:
         return "Invalid score"
-    elif score >= 90:
+    if score >= 90:
         return "Excellent"
-    elif score >= 50:
+    if score >= 50:
         return "Pass"
-    else:
-        return "Bad"
+    return "Bad"
+
 
 
 main()
