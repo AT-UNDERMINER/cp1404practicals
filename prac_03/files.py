feef23cda43671220812a2c6cc750dc378b17d19
Damien Turner <damien.turner@my.jcu.edu.au>
2025-06-02 10:46:10 +1000

    Changed variable names to be more clear

--- prac_03/files.py
+++ prac_03/files.py
@@ -5,26 +5,26 @@ File reading and writing examples using different techniques.
 
 # Q1: Ask for user's name and write it to a file
 name = input("Enter your name: ")
-name_out_file = open("name.txt", "w")
-print(name, file=name_out_file)
-name_out_file.close()
+name_output_file = open("name.txt", "w")
+print(name, file=name_output_file)
+name_output_file.close()
 
 # Q2: Read name from file and print greeting
-name_in_file = open("name.txt", "r")
-file_name = name_in_file.read().strip()
-name_in_file.close()
+name_input_file = open("name.txt", "r")
+file_name = name_input_file.read().strip()
+name_input_file.close()
 print(f"Hi {file_name}!")
 
 # Q3: Read first two numbers from numbers.txt and add them
-with open("numbers.txt", "r") as number_in_file:
-    first_number = int(number_in_file.readline())
-    second_number = int(number_in_file.readline())
+with open("numbers.txt", "r") as numbers_input_file:
+    first_number = int(numbers_input_file.readline())
+    second_number = int(numbers_input_file.readline())
     total = first_number + second_number
     print(total)
 
 # Q4: Read all numbers and calculate total
-with open("numbers.txt", "r") as number_in_file:
+with open("numbers.txt", "r") as numbers_input_file:
     total = 0
-    for line in number_in_file:
+    for line in numbers_input_file:
         total += int(line)
     print(total)
