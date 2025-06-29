b9fca06df392b3d86bc9031623fe419ec4091158
Damien Turner <damien.turner@my.jcu.edu.au>
2025-06-29 22:22:50 +1000

    Add Guitar class with get_age and is_vintage methods

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,7 +1,32 @@
 """
 Damien Turner
 CP1404 - Practical Solution
-Program description: TODO: Add description
-Estimate: TODO: Add time estimate 
-Actual: TODO: Add time actual
+Program description: Guitar class for CP1404 Intermediate Exercise.
+Estimate: 20 mins
+Actual: 28 mins
 """
+
+CURRENT_YEAR = 2022  # Constant for consistency in age calculations
+VINTAGE_AGE = 50
+
+
+class Guitar:
+    """Represent a Guitar."""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar instance."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return a string representation of a Guitar."""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        """Return the age of the guitar in years."""
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        """Return True if the guitar is 50 or more years old, else False."""
+        return self.get_age() >= VINTAGE_AGE
