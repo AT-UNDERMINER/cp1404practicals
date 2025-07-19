a30766a789b97bf19a43dcba001e580194dab32d
Damien Turner <damien.turner@my.jcu.edu.au>
2025-07-19 12:03:27 +1000

    Add display_menu to show menu options

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,7 +1,18 @@
 """
 Damien Turner
 CP1404 - Practical Solution
-Program description: TODO: Add description
-Estimate: TODO: Add time estimate 
+Program description: Project Management Program
+Estimate: 90 mins
 Actual: TODO: Add time actual
 """
+
+
+def display_menu():
+    """Display the available menu options."""
+    print("- (L)oad projects")
+    print("- (S)ave projects")
+    print("- (D)isplay projects")
+    print("- (F)ilter projects by date")
+    print("- (A)dd new project")
+    print("- (U)pdate project")
+    print("- (Q)uit")
