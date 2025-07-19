28b1b370170899c199098fbc958713283e470b4f
Damien Turner <damien.turner@my.jcu.edu.au>
2025-07-19 11:24:41 +1000

    Create Project class skeleton with attributes

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,7 +1,18 @@
 """
 Damien Turner
 CP1404 - Practical Solution
-Program description: TODO: Add description
-Estimate: TODO: Add time estimate 
+Program description: Project class for Project Management Program
+Estimate: 60 mins
 Actual: TODO: Add time actual
 """
+
+
+class Project:
+    """Represent a project with attributes."""
+
+    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
