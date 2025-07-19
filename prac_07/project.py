b12bf160d8b4ebe1c181888adbc8a263d4f9e952
Damien Turner <damien.turner@my.jcu.edu.au>
2025-07-19 11:42:14 +1000

    Add __str__, is_complete and __lt__ methods to project.py

--- prac_07/project.py
+++ prac_07/project.py
@@ -6,13 +6,30 @@ Estimate: 60 mins
 Actual: TODO: Add time actual
 """
 
+import datetime
+
 
 class Project:
-    """Represent a project with attributes."""
+    """Represent a project with attributes and helper methods."""
 
     def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        """Construct a Project from given values."""
         self.name = name
-        self.start_date = start_date
+        self.start_date = start_date  # datetime.date
         self.priority = priority
         self.cost_estimate = cost_estimate
         self.completion_percentage = completion_percentage
+
+    def __str__(self):
+        """Return a formatted string representation."""
+        start_str = self.start_date.strftime("%d/%m/%Y")
+        return (f"{self.name}, start: {start_str}, priority {self.priority}, "
+                f"estimate: ${self.cost_estimate:.2f}, completion: {self.completion_percentage}%")
+
+    def is_complete(self):
+        """Return True if project is fully complete."""
+        return self.completion_percentage >= 100
+
+    def __lt__(self, other):
+        """Enable sorting by priority (lower number = higher priority)."""
+        return self.priority < other.priority
