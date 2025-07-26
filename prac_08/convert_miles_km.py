af6690fa24c74764d320ceaa5a75eb5e7c6bc2bd
Damien Turner <damien.turner@my.jcu.edu.au>
2025-07-26 11:11:48 +1000

    Add miles to kilometers GUI layout

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,7 +1,21 @@
 """
 Damien Turner
 CP1404 - Practical Solution
-Program description: TODO: Add description
-Estimate: TODO: Add time estimate 
-Actual: TODO: Add time actual
+Program description: GUI program to convert miles to kilometres
 """
+
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class ConvertMilesToKilometres(App):
+    """Main Program - convert miles to kilometres"""
+
+    def build(self):
+        """Build the app"""
+        self.title = 'Convert Miles to Kilometres'
+        self.root = Builder.load_file('convert_miles_km.kv')
+        return self.root
+
+
+ConvertMilesToKilometres().run()
