af0c4ece5584885834dacae058c764aa4298c886
Damien Turner <damien.turner@my.jcu.edu.au>
2025-07-28 20:58:31 +1000

    Add main menu loop and basic control structure for taxi simulator

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -3,3 +3,28 @@ Damien Turner
 CP1404 - Practical Solution
 Program description: Taxi simulator program using Taxi and SilverServiceTaxi classes.
 """
+
+MENU = "q)uit, c)hoose taxi, d)rive"
+
+
+def main():
+    """Run the taxi simulator program."""
+    print("Let's drive!")
+    print(MENU)
+    choice = input(">>> ").lower()
+
+    while choice != "q":
+        if choice == "c":
+            print("c")
+        elif choice == "d":
+            print("d")
+        else:
+            print("Invalid option")
+
+        print(MENU)
+        choice = input(">>> ").lower()
+
+    print("Thank you for using the taxi simulator.")
+
+if __name__ == "__main__":
+    main()
