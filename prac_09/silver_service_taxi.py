b1e5fc34344fb38b45492a2d17d8d00bf7a2a5d8
Damien Turner <damien.turner@my.jcu.edu.au>
2025-07-28 18:36:04 +1000

    Define __init__ with fanciness and add flagfall class variable

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -3,3 +3,17 @@ Damien Turner
 CP1404 - Practical Solution
 Program description: SilverServiceTaxi class, derived from Taxi
 """
+
+from prac_09.taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    """A specialized Taxi that includes fanciness and flagfall charges."""
+    flagfall = 4.50  # Flat charge per fare
+
+    def __init__(self, name, fuel, fanciness):
+        """Initialise a SilverServiceTaxi."""
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        # Adjust the price_per_km using the class variable as a base
+        self.price_per_km = Taxi.price_per_km * fanciness
