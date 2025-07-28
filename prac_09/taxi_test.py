b4a15e5ec923d4457600b7fa920b195913fc9f08
Damien Turner <damien.turner@my.jcu.edu.au>
2025-07-28 17:27:29 +1000

    Add my_taxi object, drive taxi 40km, print taxi's details and current fare

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -4,3 +4,19 @@ CP1404 - Practical Solution
 Program description: Test the Taxi class using drive and fare-related methods.
 """
 
+from taxi import Taxi
+
+
+def main():
+    """Test the Taxi class."""
+    # Create a new taxi object
+    my_taxi = Taxi("Prius 1", 100, 1.23)
+
+    # Drive the taxi 40 km
+    my_taxi.drive(40)
+    print(my_taxi)
+    print(f"Current fare: ${my_taxi.get_fare():.2f}")
+
+
+if __name__ == "__main__":
+    main()
