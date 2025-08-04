140a99976cd2a8be637a6ad9a705c7b107232553
Damien Turner <damien.turner@my.jcu.edu.au>
2025-08-04 14:41:04 +1000

    Add main() function to ask user for page title and search using wikipedia package

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -6,3 +6,23 @@ Estimate: 30 mins
 Actual: TODO: Add time actual
 """
 
+import wikipedia
+
+
+def main():
+    """Wikipedia search program that gets and prints title, summary and URL."""
+    search_phrase = input("Enter page title: ").strip()
+
+    while search_phrase != "":
+        page = wikipedia.page(search_phrase, auto_suggest=False)
+        print(page.title)
+        print(page.summary)
+        print(page.url)
+        print()
+        search_phrase = input("Enter page title: ").strip()
+
+    print("Thank you.")
+
+
+if __name__ == "__main__":
+    main()
