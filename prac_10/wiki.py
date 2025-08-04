c755fe0a269b4e33f83f957f1215a01ed2ef7ad0
Damien Turner <damien.turner@my.jcu.edu.au>
2025-08-04 14:48:39 +1000

    Add error handling to main() function for better usability, add actual time value

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -3,21 +3,33 @@ Damien Turner
 CP1404 - Practical Solution
 Program description: Program using an API to access Wikipedia
 Estimate: 30 mins
-Actual: TODO: Add time actual
+Actual: 20 mins
 """
 
 import wikipedia
 
 
 def main():
-    """Wikipedia search program that gets and prints title, summary and URL."""
+    """Wikipedia search program with exception handling and formatted output."""
     search_phrase = input("Enter page title: ").strip()
 
     while search_phrase != "":
-        page = wikipedia.page(search_phrase, auto_suggest=False)
-        print(page.title)
-        print(page.summary)
-        print(page.url)
+        try:
+            page = wikipedia.page(search_phrase, auto_suggest=False)
+            print(page.title)
+            print(page.summary)
+            print(page.url)
+
+        except wikipedia.exceptions.PageError:
+            print(f'Page id "{search_phrase}" does not match any pages. Try another id!')
+
+        except wikipedia.exceptions.DisambiguationError as e:
+            print("We need a more specific title. Try one of the following, or a new search:")
+            print(e.options)
+
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+
         print()
         search_phrase = input("Enter page title: ").strip()
 
